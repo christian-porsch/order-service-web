09f78f84ddd5e2d5f78066e47b0d7c25868df1d8
christianp <porsc.chr@gmail.com>
2021-05-05 11:20:21 +0200

    Init spring

---
+++ src/main/java/de/neuefische/orderserviceweb/OrderServiceWebApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.orderserviceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceWebApplication.class, args);
+    }
+
+}
